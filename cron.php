1226c307e626b1a7322f2d264ccc6746eac64cb0
alabamaster1337 <alabamaster1@gmail.com>
2019-01-25 13:49:28 +0300

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,60 +1,59 @@
 <?php 
-// NEED PHPMailer 
+// need PHPMailer
 require_once 'cfg.php';
 
 $lasttime = time() - 86400/3; // 3days
-var_dump($lasttime);
+
+require 'email/src/Exception.php';
+require 'email/src/PHPMailer.php';
+require 'email/src/SMTP.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-$get = $pdo->query("SELECT * FROM amx_admins_servers admsrv JOIN amx_amxadmins amxadm WHERE amxadm.expired <= '".$lasttime."'");
-$row = $get->fetch(PDO::FETCH_ASSOC);
-//echo '<pre>';
-//print_r($row);
+$get = $pdo->query("SELECT admin_id, email, id, username, nickname, expired FROM amx_admins_servers adm JOIN amx_amxadmins amx WHERE amx.expired <= '".$lasttime."' AND adm.admin_id = amx.id");
 
-if ( $row['email'] != NULL ) {
-	require 'email/src/Exception.php';
-	require 'email/src/PHPMailer.php';
-	require 'email/src/SMTP.php';
+if ( $get != NULL ) {
+	while ( $row = $get->fetch(PDO::FETCH_ASSOC) ) {
+		$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+		try {
+		    //Server settings
+		    $mail->CharSet = 'UTF-8';
+		    $mail->SMTPDebug = 0;                                 // Включить подробный debug вывод
+		    $mail->isSMTP();                                      // Настройте почтовую программу на использование SMTP
+		    $mail->Host = 'smtp.gmail.com;smtp.gmail.com';  // Укажите основной и резервный SMTP-серверы
+		    $mail->SMTPAuth = true;                               // Включить аутентификацию SMTP
+		    $mail->Username = '';                 // SMTP username
+		    $mail->Password = '';                           // SMTP password
+		    $mail->SMTPSecure = 'ssl';                            // Включить шифрование TLS, `ssl` также разрешен
+		    $mail->Port = 465;                                    // TCP-порт для подключения
 
-	$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-	try {
-	    //Server settings
-	    $mail->CharSet = 'UTF-8';
-	    $mail->SMTPDebug = 0;                                 // Включить подробный debug вывод
-	    $mail->isSMTP();                                      // Настройте почтовую программу на использование SMTP
-	    $mail->Host = 'smtp.gmail.com;smtp.gmail.com';  // Укажите основной и резервный SMTP-серверы
-	    $mail->SMTPAuth = true;                               // Включить аутентификацию SMTP
-	    $mail->Username = '';                 // SMTP username
-	    $mail->Password = '';                           // SMTP password
-	    $mail->SMTPSecure = 'ssl';                            // Включить шифрование TLS, `ssl` также разрешен
-	    $mail->Port = 465;                                    // TCP-порт для подключения
+		    //Получатели
+		    $mail->setFrom('alabamaster1@gmail.com', 'CS Shop'); // от кого + имя
+		    $mail->addAddress($row['email']); // кому
+		    $mail->addReplyTo('alabamaster1@gmail.com'); // адрес для ответа
 
-	    //Получатели
-	    $mail->setFrom('alabamaster1@gmail.com', 'CS Shop'); // от кого + имя
-	    $mail->addAddress($row['email']); // кому
-	    $mail->addReplyTo('alabamaster1@gmail.com'); // адрес для ответа
+		    //Content
+		    $mail->isHTML(true);                                  // Set email format to HTML
+		    $mail->Subject = 'CS Shop - Привилегии подходят к концу!'; // тема сообщения
+		    
+		    // отпавка html письма
+		    $mail->Body    = "<div style='background-color:#ffb4b4;padding:10px;border-radius:10px;font-size: 14px;'>
+	            Ваши привилегии подходят к концу!<br>
+	            Продлить можно <b><a href='".$url."'' target='_blank'>тут</a></b><br>
+	            Ваш никнейм: ".$row['nickname']."</div>
+	        ";
+		    
+		    // если html не выводится
+		    $mail->AltBody = "
+				Ваши привилегии подходят к концу! | 
+				Продлить можно тут ".$url."
+			";
 
-	    //Content
-	    $mail->isHTML(true);                                  // Set email format to HTML
-	    $mail->Subject = 'CS Shop - Услуга подходит к концу!'; // тема сообщения
-	    
-	    // отпавка html письма
-	    $mail->Body    = "<div style='background-color:#ffb4b4;padding:10px;border-radius:10px;font-size: 14px;'>
-            Ваши привилегии истекают через: <b>3 дня</b>!<br>
-            Ссылка на покупку привилегий: <b><a href='".$url."'' target='_blank'>кликнуть сюда</a></b><br>
-            Ваш никнейм: ".$row['nickname']."</div>
-        ";
-	    
-	    // если html не выводится
-	    $mail->AltBody = "
-			Ваши привилегии истекают через 3 дня! | 
-			Ссылка на покупку привилегий: ".$url."
-		";
-
-	    $mail->send();
-	    echo 'Message has been sent'; // сообщение было отправлено
-	} catch (Exception $e) {
-	    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo; // Сообщение не может быть отправлено
+		    $mail->send();
+		    echo 'Message has been sent'; // сообщение было отправлено
+		} catch (Exception $e) {
+		    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo; // Сообщение не может быть отправлено
+		}
 	}
 }
